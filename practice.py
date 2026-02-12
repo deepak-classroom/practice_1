d53c0c813749d77ba5b37335a2b7712f35e6f5ef
Deepak Kumar <152460353+Gautamm17@users.noreply.github.com>
2026-02-13 00:02:14 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,26 @@
+# def table(a):
+#     for i in range(1, 11):
+#         print(a * i)
+    
+# table(5)
+
+# program to find the table of any number
+
+a = int(input("Enter Your Number; "))
+
+for i in range(1, 11):
+    print(a*i)
+# PROGRAM TO FIND THE GIVEN NUMBER AS PRIME NUMBER.
+
+# a = int(input("enter your number: "))
+
+# if a <= 1:
+#     print("not prime")
+# else:
+#     for i in range(2, a):
+#         if a % i == 0:
+#             print("not prime")
+#             break
+#     else:
+#         print("Prime number")
+
