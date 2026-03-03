c31e88c27f1b6e8baa0f2e20fde2b0d469d8a334
Deepak Kumar <deepakk2005kumar@gmail.com>
2026-03-03 12:25:50 +0530

    practise numpy day 2

---
+++ AI_PRACTICE.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+# arr_1 = np.array([1,2,3])
+# print(arr_1)
+
+# a1 = np.array([[1,2,3], [1,2,3]])
+# a2 = np.array([[1,2,3], [1,2,3]])
+
+# print(a1*a2)
+
+
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# scalar = 10
+# print(arr + scalar)
+
+# row_vector = np.array([1, 2, 3])
+# print(arr + row_vector)
+
+# col_vector = np.array([[1], [2], [3]])
+# print(arr + col_vector)
+
+# A = np.array([[1,2], [2,2]])
+# B = np.array([[5, 6], [7, 8]])
+# print(np.dot(A,B)) #multiplication of two matrix
+
+# print(A.T)
+
+# print(np.linalg.inv(A))   #inverse of matrix A
+
+# angles = np.array([0, 30, 45, 60, 90]) * np.pi / 180  # radians convert
+# print("Sin:", np.sin(angles))
+# print("Cos:", np.cos(angles))
+
+# data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print("Sum:", np.sum(data))
+# print("Sum along rows:", np.sum(data, axis=1))
+# print("Sum along columns:", np.sum(data, axis=0))
+# print("Min:", np.min(data))
+# print("Max:", np.max(data))
+# print("Std deviation:", np.std(data))
+
+# a  = np.random.rand(3,3) #generate random matrix from 0 to 1 in 3X3
+# print(a)
+
+# a = np.random.randint(1, 100, 10)
+# print(a)
+
+# a = np.random.seed(41)  #generate fix value each time a code will run.
+# print("With seed 41:", np.random.rand(3))
+
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+# np.save('my_array.npy', arr)
+
+# load_array = np.load('my_array.npy')
+# print(load_array)
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+
+#(Boolean indexing)
+bool_mask = arr > 5
+print("Elements > 5:\n", arr[bool_mask])
+
+# (Fancy indexing) - integer arrays से index करना
+rows = np.array([0, 2])
+cols = np.array([1, 3])
+print("Selected elements:", arr[rows, cols])
+
+# व्हेयर (where) कंडीशन
+condition = arr > 7
+result = np.where(condition, arr, -1)  # condition true तो arr value, false तो -1
+print("Where condition:\n", result)
